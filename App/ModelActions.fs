db5bb427fd6c6c6e8e21992ca16ced6f7b59b5e6
Ricardo Milan <ricardo@fluencypass.com>
2022-06-01 09:11:26 -0300

    refatorando

---
+++ App/ModelActions.fs
@@ -0,0 +1,60 @@
+﻿namespace Model
+
+module ModelActions =
+    open System
+    open System.IO
+    open Microsoft.ML
+    open Microsoft.ML.Transforms.Text
+
+    let createSets<'T>(ctx : MLContext, path : string) =
+        let dataView = ctx.Data.LoadFromTextFile<'T>(path, hasHeader = true)
+
+        let trainTestSplit = ctx.Data.TrainTestSplit(dataView, testFraction=0.2)
+
+        (trainTestSplit.TrainSet, trainTestSplit.TestSet)
+
+    let createDataProcessPipeline<'T>(ctx : MLContext, trainSet : IDataView, peekData : bool) =
+        let pipeline = ctx.Transforms.Text.FeaturizeText("Features", "Text")
+
+        if peekData then
+            // (OPTIONAL) Peek data (such as 2 records) in training DataView after applying the ProcessPipeline's transformations into "Features"
+            Common.ConsoleHelper.peekDataViewInConsole ctx trainSet pipeline 2 |> ignore
+
+            //Peak the transformed features column
+            Common.ConsoleHelper.peekVectorColumnDataInConsole ctx "Features" trainSet pipeline 1 |> ignore
+
+        pipeline
+
+    let createTrainingPipeline(ctx : MLContext, pipeline : TextFeaturizingEstimator)=
+        let trainer = ctx.BinaryClassification.Trainers.FastTree(labelColumnName = "Label", featureColumnName = "Features")
+        let trainingPipeline = pipeline.Append(trainer)
+        trainingPipeline
+
+    let train (trainSet : IDataView, trainingPipeline : Data.EstimatorChain<_>) =
+        printfn "=============== Training the model ==============="
+        let trainedModel = trainingPipeline.Fit(trainSet)
+        trainedModel
+
+    let evaluate (ctx : MLContext , trainedModel : Data.TransformerChain<_>, testSet : IDataView)=
+        printfn "=== Evaluating Model's accuracy with Test data ==="
+        let predictions = trainedModel.Transform testSet
+        let metrics = ctx.BinaryClassification.Evaluate(predictions, "Label", "Score")
+        metrics
+
+    let persist (ctx : MLContext, path : string, trainedModel : ITransformer, trainSet : IDataView)=
+        use fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write)
+        ctx.Model.Save(trainedModel, trainSet.Schema, fs)
+
+        printfn "The model is saved to %s" path
+
+    let testPrediction<'TStatement,'TPrediction when 'TStatement: not struct and 'TPrediction: (new: unit -> 'TPrediction) and 'TPrediction: not struct> (ctx : MLContext, sampleStatement : 'TStatement, path : string) =
+
+        use stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read)
+        let trainedModel, inputSchema = ctx.Model.Load(stream)
+
+        // Create prediction engine related to the loaded trained model
+        let predEngine= ctx.Model.CreatePredictionEngine<'TStatement, 'TPrediction>(trainedModel)
+
+        //Score
+        let resultprediction = predEngine.Predict(sampleStatement)
+        resultprediction
--- App/Program.fs
+++ App/Program.fs
@@ -2,80 +2,51 @@
 open System.IO
 open Microsoft.ML
 open DataStructures.Model
+open Model.ModelActions
 
 let baseDirectory = __SOURCE_DIRECTORY__
 let dataPath = Path.Combine(baseDirectory, "Data\\wikiDetoxAnnotated40kRows.tsv.txt")
 
 let modelPath = Path.Combine(baseDirectory, "MLModels\\SentimentModel.zip")
 
-let buildTrainEvaluateAndSaveModel (mlContext : MLContext, peekData : bool) =
-    // STEP 1: Common data loading configuration
-    let dataView = mlContext.Data.LoadFromTextFile<SentimentIssue>(dataPath, hasHeader = true)
 
-    let trainTestSplit = mlContext.Data.TrainTestSplit(dataView, testFraction=0.2)
-    let trainingDataView = trainTestSplit.TrainSet
-    let testDataView = trainTestSplit.TestSet
-
-    // STEP 2: Common data process configuration with pipeline data transformations
-    let dataProcessPipeline = mlContext.Transforms.Text.FeaturizeText("Features", "Text")
-
-    if peekData then
-        // (OPTIONAL) Peek data (such as 2 records) in training DataView after applying the ProcessPipeline's transformations into "Features"
-        Common.ConsoleHelper.peekDataViewInConsole<SentimentIssue> mlContext trainingDataView dataProcessPipeline 2 |> ignore
-
-        //Peak the transformed features column
-        Common.ConsoleHelper.peekVectorColumnDataInConsole mlContext "Features" trainingDataView dataProcessPipeline 1 |> ignore
-
-    // STEP 3: Set the training algorithm, then create and config the modelBuilder
-    let trainer = mlContext.BinaryClassification.Trainers.FastTree(labelColumnName = "Label", featureColumnName = "Features")
-    let trainingPipeline = dataProcessPipeline.Append(trainer)
-
-    // STEP 4: Train the model fitting to the DataSet
-    printfn "=============== Training the model ==============="
-    let trainedModel = trainingPipeline.Fit(trainingDataView)
-
-    // STEP 5: Evaluate the model and show accuracy stats
-    printfn "===== Evaluating Model's accuracy with Test data ====="
-    let predictions = trainedModel.Transform testDataView
-    let metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label", "Score")
-
-    // STEP 6: Save/persist the trained model to a .ZIP file
-    use fs = new FileStream(modelPath, FileMode.Create, FileAccess.Write, FileShare.Write)
-    mlContext.Model.Save(trainedModel, trainingDataView.Schema, fs)
-
-    printfn "The model is saved to %s" modelPath
-
-// (OPTIONAL) Try/test a single prediction by loding the model from the file, first.
-let testSinglePrediction (mlContext : MLContext) =
-    let sampleStatement = { Label = false; Text = "This is a very rude movie" }
-
-    use stream = new FileStream(modelPath, FileMode.Open, FileAccess.Read, FileShare.Read)
-    let trainedModel,inputSchema = mlContext.Model.Load(stream)
-
-    // Create prediction engine related to the loaded trained model
-    let predEngine= mlContext.Model.CreatePredictionEngine<SentimentIssue, SentimentPrediction>(trainedModel)
-
-    //Score
-    let resultprediction = predEngine.Predict(sampleStatement)
-
-    printfn "=============== Single Prediction  ==============="
-    printfn
-        "Text: %s | Prediction: %s sentiment | Probability: %f"
-        sampleStatement.Text
-        (if Convert.ToBoolean(resultprediction.Prediction) then "Negative" else "Positive")
-        resultprediction.Probability
-    printfn "=================================================="
 
 //Create MLContext to be shared across the model creation workflow objects
 //Set a random seed for repeatable/deterministic results across multiple trainings.
 let mlContext = MLContext(seed = Nullable 1)
 
 // Create, Train, Evaluate and Save a model
-buildTrainEvaluateAndSaveModel(mlContext, false)
+// STEP 1: Common data loading configuration
+let (trainSet, testSet) = createSets<SentimentIssue>(mlContext, dataPath)
+
+// STEP 2: Common data process configuration with pipeline data transformations
+let dataProcessPipeline = createDataProcessPipeline<SentimentIssue>(mlContext, trainSet, false)
+
+// STEP 3: Set the training algorithm, then create and config the modelBuilder
+let trainingPipeline = createTrainingPipeline(mlContext, dataProcessPipeline)
+
+// STEP 4: Train the model fitting to the DataSet
+let trainedModel = train(trainSet, trainingPipeline)
+
+// STEP 5: Evaluate the model and show accuracy stats
+let metrics = evaluate(mlContext, trainedModel, testSet)
+
+// STEP 6: Save/persist the trained model to a .ZIP file
+persist(mlContext, modelPath, trainedModel, trainSet)
+
 Common.ConsoleHelper.consoleWriteHeader "=============== End of training processh ==============="
 
 // Make a single test prediction loding the model from .ZIP file
-testSinglePrediction mlContext
+let sampleStatement = { Label = false; Text = "This is a very rude movie" }
+let resultprediction = testPrediction(mlContext, sampleStatement, modelPath)
+
+printfn "=============== Single Prediction  ==============="
+printfn
+    "Text: %s | Prediction: %s sentiment | Probability: %f"
+    sampleStatement.Text
+    (if Convert.ToBoolean(resultprediction.Prediction) then "Negative" else "Positive")
+    resultprediction.Probability
+printfn "=================================================="
 
 Common.ConsoleHelper.consoleWriteHeader "=============== End of process, hit any key to finish ==============="
 Common.ConsoleHelper.consolePressAnyKey()
